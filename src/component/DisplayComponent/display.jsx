b36807d14bb14ab7eb65fae23313d40234f3ed0f
tawseef <tawseefalam1@gmail.com>
2024-08-09 12:36:45 +0530

    set the url

--- src/component/DisplayComponent/display.jsx
+++ src/component/DisplayComponent/display.jsx
@@ -7,11 +7,11 @@ import Card from "../displayCard/card";
 import axios from "axios";
 import BannerCard from "../bannerCard/bannerCard";
 
-export const API = "http://localhost:8082/v1/users";
-export const FriendAPI = "http://localhost:8082/v1/friend";
+// export const API = "http://localhost:8082/v1/users";
+// export const FriendAPI = "http://localhost:8082/v1/friend";
 
-// export const API = "https://autonomize-backend-ym55.onrender.com/v1/users";
-// export const FriendAPI = "https://autonomize-backend-ym55.onrender.com/v1/friend";
+export const API = "https://autonomize-backend-ym55.onrender.com/v1/users";
+export const FriendAPI = "https://autonomize-backend-ym55.onrender.com/v1/friend";
 
 
 function Display() {
